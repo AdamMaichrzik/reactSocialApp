bd8a363266d42652d32aa521b225c9497120a7bb
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-22 13:15:59 +0200

    Topbar component created

--- social-app/src/App.js
+++ social-app/src/App.js
@@ -1,7 +1,7 @@
+import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
-import {Person} from "@material-ui/icons"
 
 function App() {
-	return <Person />;
+	return <Topbar />;
 }
 export default App;
