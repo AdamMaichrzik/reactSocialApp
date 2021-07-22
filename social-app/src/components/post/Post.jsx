2e436bf190ba2fa3488158a9a97afe130ff3d71d
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-22 15:59:16 +0200

    Post html tags with username, date and icons

--- social-app/src/components/post/Post.jsx
+++ social-app/src/components/post/Post.jsx
@@ -1,9 +1,23 @@
-import "./post.css"
+import "./post.css";
+import { MoreVert } from "@material-ui/icons";
 
 export default function Post() {
-    return (
-        <div>
-            Post
-        </div>
-    )
+	return (
+		<div className="post">
+			<div className="postWrapper">
+				<div className="postTop">
+					<div className="postTopLeft">
+						<img className="postProfileImg" src="assets/person/8.jpeg" alt="" />
+						<span className="postUsername">Adam Maichrzik</span>
+						<span className="postDate">5 mins ago</span>
+					</div>
+					<div className="postTopRight">
+						<MoreVert />
+					</div>
+				</div>
+				<div className="postCenter"></div>
+				<div className="postBottom"></div>
+			</div>
+		</div>
+	);
 }
