f78ce54d198e9ab8fb6671c119fec1657c112a8e
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-22 16:09:51 +0200

    Post like icons and like counter

--- social-app/src/components/post/Post.jsx
+++ social-app/src/components/post/Post.jsx
@@ -16,10 +16,17 @@ export default function Post() {
 					</div>
 				</div>
 				<div className="postCenter">
-                    <span className="postText">Hey! It's my first post!</span>
-                    <img className="postImg" src="assets/post/1.jpeg" alt=""/>
+					<span className="postText">Hey! It's my first post!</span>
+					<img className="postImg" src="assets/post/1.jpeg" alt="" />
+				</div>
+				<div className="postBottom">
+                    <div className="postBottomLeft">
+                        <img className="likeIcon" src="assets/like.png" alt=""/>
+                        <img className="likeIcon" src="assets/heart.png" alt=""/>
+                        <span className="postLikeCounter">30 people like it</span>
+                    </div>
+                    <div className="postBottomRight"></div>
                 </div>
-				<div className="postBottom"></div>
 			</div>
 		</div>
 	);
