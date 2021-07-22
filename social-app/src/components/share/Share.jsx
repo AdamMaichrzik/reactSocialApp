e28c242acb6fd5d9c3dcefe75b2ac5d007aad26c
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-22 15:32:35 +0200

    Share html tags created

--- social-app/src/components/share/Share.jsx
+++ social-app/src/components/share/Share.jsx
@@ -1,9 +1,24 @@
-import "./share.css"
+import "./share.css";
+import {PermMedia } from "@material-ui/icons";
 
 export default function Share() {
-    return (
-        <div>
-            hey its share
-        </div>
-    )
+	return (
+		<div className="share">
+			<div className="shareWrapper">
+				<div className="shareTop">
+					<img className="shareProfileImg" src="assets/person/8.jpeg" alt="" />
+					<input placeholder="What is in your mind?" className="shareInput" />
+				</div>
+                <hr className="shareHr"/>
+				<div className="shareBottom">
+                    <div className="shareOptions">
+                        <div className="shareOption">
+                            <PermMedia className="shareIcon"/>
+                            <span className="shareOptionText">Photo Video</span>
+                        </div>
+                    </div>
+                </div>
+			</div>
+		</div>
+	);
 }
