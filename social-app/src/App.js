a8b78e6a99165f91f2869f9cb7900ed105515d46
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-23 13:33:01 +0200

    App showing home page

--- social-app/src/App.js
+++ social-app/src/App.js
@@ -1,9 +1,6 @@
-//import Home from "./pages/home/Home";
-//import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-//import Profile from "./pages/profile/Profile";
+import Home from "./pages/home/Home";
 
 function App() {
-	return <Register />;
+	return <Home />;
 }
 export default App;
