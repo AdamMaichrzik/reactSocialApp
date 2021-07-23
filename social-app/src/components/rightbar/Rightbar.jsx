ed4a0900418d739d09bb83abb3a8155230a0cc2f
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-23 10:03:54 +0200

    Dot in the end of the sentence

--- social-app/src/components/rightbar/Rightbar.jsx
+++ social-app/src/components/rightbar/Rightbar.jsx
@@ -6,7 +6,7 @@ export default function Rightbar() {
             <div className="rightbarWrapper">
                 <div className="birthdayContainer">
                     <img className="birthdayImg" src="assets/gift.png" alt=""/>
-                    <span className="birthdayText"><b>Pola Foster</b> and <b>3 other friends</b> have a birthday today</span>
+                    <span className="birthdayText"><b>Pola Foster</b> and <b>3 other friends</b> have a birthday today.</span>
                 </div>
             </div>
         </div>
