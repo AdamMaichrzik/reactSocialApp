4f63c39bfcf9745efdf5bca797bf301941175cc7
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-23 11:46:42 +0200

    Profile page html components

--- social-app/src/pages/profile/Profile.jsx
+++ social-app/src/pages/profile/Profile.jsx
@@ -1,5 +1,25 @@
 import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
 
 export default function Profile() {
-	return <div></div>;
+	return (
+		<div>
+			<>
+				<Topbar />
+				<div className="profile">
+					<Sidebar />
+					<div className="profileRight">
+						<div className="profileRightTop">Test top </div>
+						<div className="profileRightBottom">
+							<Feed />
+							<Rightbar />
+						</div>
+					</div>
+				</div>
+			</>
+		</div>
+	);
 }
