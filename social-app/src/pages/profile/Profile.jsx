92c16553d894d8c57566ba5c3b287b2e03bf5cf9
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-23 12:25:27 +0200

    Profile website html tags

--- social-app/src/pages/profile/Profile.jsx
+++ social-app/src/pages/profile/Profile.jsx
@@ -12,7 +12,24 @@ export default function Profile() {
 				<div className="profile">
 					<Sidebar />
 					<div className="profileRight">
-						<div className="profileRightTop">Test top </div>
+						<div className="profileRightTop">
+							<div className="profileCover">
+								<img
+									className="profileCoverImg"
+									src="assets/post/4.jpeg"
+									alt=""
+								/>
+								<img
+									className="profileUserImg"
+									src="assets/person/8.jpeg"
+									alt=""
+								/>
+							</div>
+                            <div className="profileInfo">
+                                <h4 className="profileInfoName">Adam Maichrzik</h4>
+                                <span className="profileInfoDesc">Hello. It's me, Adam! </span>
+                            </div>
+						</div>
 						<div className="profileRightBottom">
 							<Feed />
 							<Rightbar />
